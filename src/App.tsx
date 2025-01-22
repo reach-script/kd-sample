48aeb7538d42d19c26600a873d46523561eb73b6
reachscript-jak <123ja.k123@gmail.com>
2025-01-22 10:13:26 +0900

    chore: 不要なAppコンポーネント削除&suspenseの粒度修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { Error1 } from "./sample/suspense/Error1";
 import { Loading1 } from "./sample/suspense/Loading1";
 import { Loading2 } from "./sample/suspense/Loading2";
 import { Suspense1 } from "./sample/suspense/Suspense1";
@@ -10,12 +12,17 @@ export const App = () => {
     <div>
       <h2>サンプルコードを記載しています</h2>
       <WrapperCondition />
-      <Suspense fallback={<Loading1 />}>
-        <Suspense1 />
+      {/* 共通のエラー処理やローディング処理は↓のように定義できます */}
+      <ErrorBoundary fallback={<Error1 />}>
+        <Suspense fallback={<Loading1 />}>
+          <Suspense1 />
+        </Suspense>
+        {/* Loadingの粒度を適切に分割したい場合は適宜Suspenseを設定する */}
+        {/* Suspense2のAPI取得完了を待たずSuspense1コンポーネントのレンダリングが出来ている例です */}
         <Suspense fallback={<Loading2 />}>
           <Suspense2 />
         </Suspense>
-      </Suspense>
+      </ErrorBoundary>
     </div>
   );
 };
